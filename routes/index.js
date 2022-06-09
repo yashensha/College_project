400881f21ca31a02240e93372b08d2c265eba622
yashensha <m.yashensha@gmail.com>
2022-06-09 15:52:39 +0530

    second

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+router.get("/signup", (req, res) => {
+  res.json("signup page");
+});
+
+// router.post("/signup", (req, res) => {
+//   console.log("signup post");
+//   res.json("foo");
+// });
+
+router.post("/signup", (req, res) => {
+  console.log("data", req.body);
+  console.log("signup post");
+  res.json("foo");
+});
+
+router.get("/admin");
 
 module.exports = router;
