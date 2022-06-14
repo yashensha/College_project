e73fc02fef8cd81fc4e2821801a8ebff93840ff0
Noor Mohammed B <noormohammedb@protonmail.com>
2022-06-14 20:46:15 +0530

    Monthly expense
    expense schema
    submit monthly expense
    get monthly expense

---
+++ models/expense_db.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const expence = new mongoose.Schema(
+  {
+    month: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    totalMeal: {
+      type: Number,
+      required: true,
+    },
+    cost: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+
+module.exports = mongoose.model("expence", expence);
